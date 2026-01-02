dc69f856490f1effb3961261920397295d06e380
xed-joestar <abizermhowwala@gmail.com>
2026-01-02 21:42:42 +0530

    added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import SignUp from "./pages/auth/SignUp";
+import Dashboard from "./pages/admin/Dashboard";
+import ManageTasks from "./pages/admin/ManageTasks";
+import ManageUsers from "./pages/admin/ManageUsers";
+import CreateTask from "./pages/admin/CreateTask";
+import PrivateRoute from "./routes/PrivateRoute";
+import UserDashboard from "./pages/user/UserDashboard";
+import TaskDetails from "./pages/user/TaskDetails";
+import MyTasks from "./pages/user/MyTasks";
 const App = () => {
-  return <div className="text-red-500">Task Manager App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/users" element={<ManageUsers />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+          </Route>
+
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route path="/user/task-details/:id" element={<TaskDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
