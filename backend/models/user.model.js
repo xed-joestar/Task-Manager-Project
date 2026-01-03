8b709c5ae70d59062db18a0ab56a13e2563910be
xed-joestar <abizermhowwala@gmail.com>
2026-01-03 21:15:12 +0530

    task model

---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    profileImageUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
