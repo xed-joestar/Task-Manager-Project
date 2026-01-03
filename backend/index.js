c1438241053c7717d698ac76660b595e9e13641b
xed-joestar <abizermhowwala@gmail.com>
2026-01-03 20:44:58 +0530

    connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,31 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+const app = express(); // ✅ initialize FIRST
+
 // middleware to handle cors
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization "],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
